e55e0cef16d3da10931fed9f555646d52b1e87ab
Ecerea <Flightfire@gmail.com>
2018-07-15 06:56:58 -0600

    Add MySQLProvider Package

--- Package.swift
+++ Package.swift
@@ -7,9 +7,10 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/mysql-provider.git", from: "2.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor","FluentMySQL"]),
+        .target(name: "App", dependencies: ["Vapor","FluentMySQL", "MySQLProvider"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
