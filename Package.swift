2849c6d15a49c5f812f1fc36a9fcbde856ada9b8
Ecerea <Flightfire@gmail.com>
2018-07-16 05:49:29 -0600

    Delete MYSQLProvider Package

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor/mysql-provider.git", from: "2.0.0")
+        //.package(url: "https://github.com/vapor/mysql-provider.git", from: "2.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor","FluentMySQL", "MySQLProvider"]),
