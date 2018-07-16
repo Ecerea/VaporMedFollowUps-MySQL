7098ae13e7b999b9363272c9c6cedc4e6a495ddd
Ecerea <Flightfire@gmail.com>
2018-07-16 06:52:19 -0600

    Trying database config from discord

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,14 +6,13 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     /// Register providers first
     try services.register(FluentMySQLProvider())
     
-    let transportConfig = MySQLTransportConfig.unverifiedTLS
+    //let transportConfig = MySQLTransportConfig.unverifiedTLS
     let mysqlConfig = MySQLDatabaseConfig(
-        hostname: "172.20.0.248",
+        hostname: Environment.get("DATABASE_HOSTNAME") ?? "localhost",
         port: 3306,
-        username: "3DzyCwM6cIP2QJ8@10.0.10.71",
-        password: "HartlEnterprises",
-        database: "3DzyCwM6cIP2QJ8",
-        transport: transportConfig
+        username: Environment.get("DATABASE_USER") ?? "vapor",
+        password: Environment.get("DATABASE_PASSWORD") ?? "password",
+        database: Environment.get("DATABASE_DB") ?? "vapor"
     )
     services.register(mysqlConfig)
 
